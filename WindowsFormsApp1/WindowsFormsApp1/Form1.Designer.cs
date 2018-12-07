bcae09533f56ecfe6a131034ded438a78348dc72
Tzoe <y.zoeshi.1999@gmail.com>
2018-12-08 00:36:05 +0900

    upload

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Airi
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Airi));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(250, 280);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(188, 208);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(250, 241);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(188, 19);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Airi
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(450, 500);
+            this.ControlBox = false;
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Cursor = System.Windows.Forms.Cursors.AppStarting;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.Location = new System.Drawing.Point(1458, 580);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Airi";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Form1";
+            this.TransparencyKey = System.Drawing.SystemColors.Control;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Airi : Form
+    {
+        public Airi()
+        {
+            InitializeComponent();
+            textBox1.Visible = false;
+            this.Activate();
+            this.Icon = new System.Drawing.Icon(@"D:\Projects\airi\WindowsFormsApp1\WindowsFormsApp1\airi.ico");
+        }
+
+        //Form1のLoadイベントハンドラ
+        private void Form1_Load(object sender, System.EventArgs e)
+        {
+            pictureBox1.MouseDown +=
+                new MouseEventHandler(Form1_MouseDown);
+            pictureBox1.MouseMove +=
+                new MouseEventHandler(Form1_MouseMove);
+        }
+
+        //マウスのクリック位置を記憶
+        private Point mousePoint;
+
+        //Form1のMouseDownイベントハンドラ
+        //マウスのボタンが押されたとき
+        private void Form1_MouseDown(object sender,
+            System.Windows.Forms.MouseEventArgs e)
+        {
+            if ((e.Button & MouseButtons.Left) == MouseButtons.Left)
+            {
+                //位置を記憶する
+                mousePoint = new Point(e.X, e.Y);
+            }
+        }
+
+        //Form1のMouseMoveイベントハンドラ
+        //マウスが動いたとき
+        private void Form1_MouseMove(object sender,
+            System.Windows.Forms.MouseEventArgs e)
+        {
+            if ((e.Button & MouseButtons.Left) == MouseButtons.Left)
+            {
+                this.Left += e.X - mousePoint.X;
+                this.Top += e.Y - mousePoint.Y;
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyValue == 18 )
+            {
+                textBox1.Visible = !textBox1.Visible;
+                if (textBox1.Visible == true)
+                {
+                    textBox1.Focus();
+                    this.TopMost = true;
+                }
+                else
+                {
+                    this.TopMost = false;
+                }
+            }
+    
+            if( (textBox1.Visible == true) && (e.KeyValue == 13))
+            {
+                if(textBox1.Text == "chrome")
+                {
+                    Process.Start(@"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe");
+                }
+                else if(textBox1.Text == "memo")
+                {
+                    Process.Start(@"C:\Windows\system32\notepad.exe");
+                }
+                else if(textBox1.Text == "peint"){
+                    Process.Start(@"C:\Windows\system32\mspaint.exe");
+                }
+                else if(textBox1.Text == "gimp")
+                {
+                    Process.Start(@"C:\Program Files\GIMP 2\bin\gimp-2.10.exe");
+                }
+                else if(textBox1.Text == "music")
+                {
+                    Process.Start(@"D:\\music\\Playlists\\fav.zpl");
+                }
+                textBox1.Text = "\n";
+                textBox1.Visible = false;
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
